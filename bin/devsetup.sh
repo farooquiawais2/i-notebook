c113091ec8315a377488d5fe34c7f12222589f89
Awais Farooqui <farooquiawais2@gmail.com>
2023-10-14 16:54:03 +0500

    add devsetup file

---
+++ bin/devsetup.sh
@@ -0,0 +1,13 @@
+echo "Installing node modules..."
+  npm install
+  echo "Installed successfully"
+
+
+echo "Building frondend app..."
+  npm run build
+  echo "build frondend app successfully"
+
+echo "Running server and frondend..."
+  npm run both
+  
+echo "devsetup successfully done"
